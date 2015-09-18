270394860dfccc724f112ddfb6dab05d24c7a736
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-09-18 17:25:45 -0500

    added sad path test for browsing inventory

--- spec/features/visitor_can_browse_inventory_spec.rb
+++ spec/features/visitor_can_browse_inventory_spec.rb
@@ -1,15 +1,32 @@
 require "rails_helper"
-
+require "spec_helper"
 RSpec.feature "Visitor can browse inventory" do
 
   scenario "test products page" do
-    Product.create!(name: "Product 1")
-    Product.create!(name: "Product 2")
+    visit root_path
 
-    visit "/"
+    click_on "New Product"
+    expect(page).to have_content "Add Product"
+    fill_in "Name", with: "Lace"
+    fill_in "Description", with: "Beautifiul Yellow Lace"
+    fill_in "Price", with: "10"
+    fill_in "Color", with: "Yellow"
+    fill_in "Quantity", with: "50"
 
-    expect(page).to have_content "Product 1"
-    expect(page).to have_content "Product 2"
+    click_on "Add"
+    current_path.should == products_path
+    page.should have_css("p", text: "Lace")
+  end
+
+  scenario "sad path" do
+    visit root_path
+
+    click_on "New Product"
+    expect(page).to have_content "Add Product"
+    fill_in "Name", with: ""
+    fill_in "Description", with: ""
+    click_on "Add"
+    expect(page).to have_content("Product could not be saved. Please fix the errors to continue.")
   end
 
 end
